a5529ff73fbe3c0b0982ec75a76454fddcc61b29
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-05-24 14:52:31 +0200

    progress!

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        // 1. If the length of the provided string is 0, then return the default of
+        //    Person.
+        if s.len() == 0 {
+            return Person::default();
+        }
+        // 2. Split the given string on the commas present in it.
+        let parts: Vec<&str> = s.split(',').collect();
+        // 3. Extract the first element from the split operation and use it as the name.
+        let name = parts.get(0).unwrap_or(&"").to_string();
+        // 4. If the name is empty, then return the default of Person.
+        if name.is_empty() {
+            return Person::default();
+        }
+        // 5. Extract the other element from the split operation and parse it into a
+        //    `usize` as the age.
+        let age = parts.get(1).unwrap_or(&"").parse::<usize>();
+        // If while parsing the age, something goes wrong, then return the default of
+        // Person Otherwise, then return an instantiated Person object with the results
+        match age {
+            Ok(age) => Person { name, age },
+            Err(_) => Person::default(),
+        }
     }
 }
 
